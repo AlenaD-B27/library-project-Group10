554468489ee5b404f9966da492776ed547b14548
AlenaD-B27 <lenador.18@gmail.com>
2022-12-02 17:42:14 -0600

    Cleaned the snippets for US-4

--- src/test/java/com/LibraryCT/step_definitions/Alena_Step_Defs.java
+++ src/test/java/com/LibraryCT/step_definitions/Alena_Step_Defs.java
@@ -7,22 +7,18 @@ import io.cucumber.java.en.When;
 public class Alena_Step_Defs {
     @Given("I login as a {string} by Alena")
     public void i_login_as_a_by_alena(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+
     }
     @Given("I navigate to {string} page by Alena")
     public void i_navigate_to_page_by_alena(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+
     }
     @When("I open book {string} by Alena")
     public void i_open_book_by_alena(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+
     }
     @Then("book information must match the Database by Alena")
     public void book_information_must_match_the_database_by_alena() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+
     }
 }
