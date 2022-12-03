2baa56bd203f705a1641b4656f88a762bc784606
olhasoroka <olly0296@gmail.com>
2022-12-03 14:34:39 -0600

    Updated my step_def class and US-3-Olga id done!

--- src/test/java/com/LibraryCT/step_definitions/Olga_Step_Defs.java
+++ src/test/java/com/LibraryCT/step_definitions/Olga_Step_Defs.java
@@ -9,6 +9,7 @@ import com.LibraryCT.utilities.Driver;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.junit.Assert;
 
 import java.util.List;
 
@@ -16,7 +17,8 @@ public class Olga_Step_Defs {
 
     LoginPage loginPage = new LoginPage();
     BookPage bookPage = new BookPage();
-    List<String> allCategories;
+
+    List<String> actualAllCategories;
 
 
     @Given("I login as a {string} by Olga")
@@ -33,15 +35,21 @@ public class Olga_Step_Defs {
     }
     @When("I take all book categories in UI by Olga")
     public void i_take_all_book_categories_in_ui_by_olga() {
-
+        actualAllCategories = BrowserUtil.getAllSelectOptions(bookPage.mainCategoryElement);
+        //actualAllCategories.remove(0);
+        //System.out.println("actualAllCategories = " + actualAllCategories);
 
     }
     @When("I execute query to get book categories by Olga")
     public void i_execute_query_to_get_book_categories_by_olga() {
+        actualAllCategories.remove(0);
+        System.out.println("actualAllCategories = " + actualAllCategories);
+
 
     }
     @Then("verify book categories must match book_categories table from db by Olga")
-    public void verify_book_categories_must_match_book_categories_table_from_db_by_olga() {
+    public void verify_book_categories_must_match_book_categories_table_from_db_by_olga(List<String> expectedAllCategories) {
+        Assert.assertEquals(expectedAllCategories,actualAllCategories);
 
     }
 }
