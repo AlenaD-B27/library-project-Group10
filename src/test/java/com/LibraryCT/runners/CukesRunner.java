1d8d24c54398886382f2b40fe19f2bed0283c3ca
olhasoroka <olly0296@gmail.com>
2022-12-03 13:40:20 -0600

    changed on "false" in dryRun CukesRunner

--- src/test/java/com/LibraryCT/runners/CukesRunner.java
+++ src/test/java/com/LibraryCT/runners/CukesRunner.java
@@ -15,7 +15,7 @@ import org.junit.runner.RunWith;
         },
         features = "src/test/resources/features" ,
         glue = "com/LibraryCT/step_definitions",
-        dryRun = true,
+        dryRun = false,
         tags = "@Olga"
 )
 public class CukesRunner {
