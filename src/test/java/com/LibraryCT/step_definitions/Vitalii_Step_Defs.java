13a41b76a9eda2fcd7c73ca881f16483931defc4
Vitalii <vtrushch@gmail.com>
2022-12-05 19:38:56 -0600

    start user story 2

--- src/test/java/com/LibraryCT/step_definitions/Vitalii_Step_Defs.java
+++ src/test/java/com/LibraryCT/step_definitions/Vitalii_Step_Defs.java
@@ -63,6 +63,9 @@ public class Vitalii_Step_Defs {
     }
     @When("I execute query to find most popular book genre by Vitalii")
     public void i_execute_query_to_find_most_popular_book_genre_by_vitalii() {
+        String query = "select bc.name, count(*) from book_borrow bb inner join books b on bb.book_id = b.id inner join book_categories bc on b.book_category_id = bc.id group by bc.name order by count(*) desc";
+        DB_Util.runQuery(query);
+
 
     }
 
