57d712c55a29c331f62b12a9c47edb5dd1b9f882
Maria <kitmaria.2015@gmail.com>
2022-12-06 02:48:41 +0100

    finish the implementation

--- src/test/java/com/LibraryCT/step_definitions/Maria_Step_Defs.java
+++ src/test/java/com/LibraryCT/step_definitions/Maria_Step_Defs.java
@@ -1,16 +1,24 @@
 package com.LibraryCT.step_definitions;
 
-import com.LibraryCT.pages.BookPage;
-import com.LibraryCT.pages.DashBoardPage;
-import com.LibraryCT.pages.LoginPage;
+import com.LibraryCT.pages.*;
 import com.LibraryCT.utilities.BrowserUtil;
+import com.LibraryCT.utilities.DB_Util;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.junit.Assert;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class Maria_Step_Defs {
     DashBoardPage dashBoardPage = new DashBoardPage();
     BookPage bookPage = new BookPage();
+    UserPage userPage = new UserPage();
+    BorrowedBookPage borrowedBookPage = new BorrowedBookPage();
+    String bookName;
     @Given("I login as a {string} by Maria")
     public void i_login_as_a_by_maria(String student) {
         new LoginPage().login(student);
@@ -23,20 +31,60 @@ public class Maria_Step_Defs {
         BrowserUtil.waitFor(3);
     }
     @Given("I search book name called {string} by Maria")
-    public void i_search_book_name_called_by_maria(String bookName) {
+    public void i_search_book_name_called_by_maria(String name) {
+        bookName=name;
         bookPage.search.sendKeys(bookName);
     }
     @When("I click Borrow Book by Maria")
     public void i_click_borrow_book_by_maria() {
-
+        BrowserUtil.selectByVisibleText(userPage.numberOfUsersDropdown,"500");
+        BrowserUtil.waitFor(3);
+        bookPage.borrowBook.click();
     }
     @Then("verify that book is shown in {string} page by Maria")
-    public void verify_that_book_is_shown_in_page_by_maria(String string) {
-
+    public void verify_that_book_is_shown_in_page_by_maria(String borrowingBookModule) {
+        dashBoardPage.navigateModule(borrowingBookModule);
+        BrowserUtil.waitFor(3);
+        Assert.assertTrue(BrowserUtil.getElementsText(borrowedBookPage.allBorrowedBooksName).contains(bookName));
 
     }
     @Then("verify logged student has same book in database by Maria")
     public void verify_logged_student_has_same_book_in_database_by_maria() {
 
+       Map<String,String> verifyInfoBook = new HashMap<>();
+        for(int i = 0 ; i < bookPage.tableHeaderCells.size(); i++){
+            String getKey = bookPage.tableHeaderCells.get(i).getText();
+            String getValue = bookPage.allCell.get(i).getText();
+            verifyInfoBook.put(getKey.toLowerCase(),getValue);
+        }
+
+        String query = "select full_name,b.name,bb.borrowed_date from users u\n" +
+                "inner join book_borrow bb on u.id = bb.user_id\n" +
+                "inner join books b on bb.book_id = b.id\n" +
+                "where full_name='Test Student 35'\n" +
+                "order by 3 desc";
+        DB_Util.runQuery(query);
+   /*  List<String> allActualInfo = new ArrayList<>();
+        for(int i = 0; i < bookPage.allCell.size(); i++){
+        String info = bookPage.allCell.get(i).getText();
+        allActualInfo.add(info.toLowerCase());
+        List <String> expectedInfoBook = DB_Util.getRowDataAsList(1);
+
+        for(int j = 0; j < expectedInfoBook.size(); j++){
+            if(allActualInfo.get(i).contains(bookName)){
+                Assert.assertEquals(expectedInfoBook.get(i), allActualInfo.get(j));
+            }
+        }
+
+    }*/
+
+        Map <String,String> expectedInfo = DB_Util.getRowMap(1);
+        for (String eachKey : expectedInfo.keySet()) {
+           if(verifyInfoBook.containsKey(eachKey)){
+                Assert.assertEquals(expectedInfo.get(eachKey),verifyInfoBook.get(eachKey));
+            }
+        }
+
     }
 }
+
