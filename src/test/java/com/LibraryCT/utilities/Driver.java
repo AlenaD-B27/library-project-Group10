eb4fdc11dd34c22ab0d520ed535cb6d30f294e30
Jennifer Radina <theradinas@gmail.com>
2022-12-05 18:33:41 -0600

    Updated Driver

--- src/test/java/com/LibraryCT/utilities/Driver.java
+++ src/test/java/com/LibraryCT/utilities/Driver.java
@@ -54,7 +54,7 @@ public class Driver {
                 case "remote-firefox":
                     try {
                         // assign your grid server address
-                        String gridAddress = "USE.YOUR.OWN.INSTANCE"; //TODO
+                        String gridAddress = "54.198.193.9"; //TODO
                         URL url = new URL("http://"+ gridAddress + ":4444/wd/hub");
                         DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
                         desiredCapabilities.setBrowserName("firefox");
